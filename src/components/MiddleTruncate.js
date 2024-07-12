7eae8484bf3ed13ff2c6a6909183d102a9bb2548
LucaRanieri96 <luca.ranieri.4@gmail.com>
2024-07-12 16:50:22 +0200

    minor fixes

--- src/components/MiddleTruncate.js
+++ src/components/MiddleTruncate.js
@@ -5,6 +5,7 @@ function MiddleEllipsis({ text }) {
   const [maxLength, setMaxLength] = useState(text.length);
 
   useEffect(() => {
+    // funzione che aggiorna il maxLenght per il testo
     const updateMaxLength = () => {
       if (containerRef.current) {
         const parentWidth = containerRef.current.parentElement.offsetWidth;
@@ -19,7 +20,7 @@ function MiddleEllipsis({ text }) {
         setMaxLength(newMaxLength);
       }
     };
-
+    // richiamo la funzione
     updateMaxLength();
     const resizeObserver = new ResizeObserver(updateMaxLength);
 
@@ -52,7 +53,7 @@ function MiddleEllipsis({ text }) {
 
 function MiddleTruncate() {
   const items = [
-    "Eiusmod elit laborum commodo aute nisi velit sunt enim occaecat ad sunt enim occaecat ad sunt enim occaecat ad ah ah ah ah id te eiusmod commodo fugiat laborum. Exercitation consequat minim est amet occaecat eu ad aute magna cupidatat duis qui labore noa a a a a an a A A A A A.",
+    "Eiusmod elit laborum commodo aute nisi velit sunt enim occaecat ad sunt enim occaecat ad sunt enim occaecat ad ah ah ah ah id te eiusmod commodo fugiat laborum. Exercitation consequat minim est amet occaecat eu ad aute magna cupidatat duis qui labore noa a a a a an a A A A A A. a a a a a a a a a a a a a a a a a a a a a a a a a a a a",
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
@@ -63,7 +64,6 @@ function MiddleTruncate() {
       <div className='flex flex-col gap-2'>
         {items.map((item) => (
           <div key={item.substring(0, 10)} className="bg-slate-200 rounded h-[165px]">
-            {/* Testo con ellissi */}
             <div className="flex justify-start h-full p-2">
               <MiddleEllipsis text={item} />
             </div>
